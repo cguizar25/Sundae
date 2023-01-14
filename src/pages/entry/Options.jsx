76fd65a79e34dd2792743175bb36d57043f8687b
Christian <c-guizar@hotmail.com>
2023-01-14 04:42:23 -0800

    feat(topping): added a toppings options component

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,27 +1,29 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
-import ScoopOption from 'ScoopOption';
+import Row from "react-bootstrap/Row";
+import ScoopOption from './ScoopOption';
+import ToppingOption from './ToppingOption';
 
-export default const Options = ({ optionType }) => {
-  const [ items, setItems ] = useState([]);
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
 
-  useEffect(()= {
-    axios.get(`http://localhost:3030/${optionType
-      .then(response => setItems(response.data));
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
       .catch((error) => {
-
-      })
+      });
   }, [optionType]);
 
-  const ItemComponent = optionType ==='scoops' ? ScoopOption : null;
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
-  const optionItems = items.map(item => {
+  const optionItems = items.map((item) => (
     <ItemComponent
       key={item.name}
       name={item.name}
       imagePath={item.imagePath}
     />
-  });
+  ));
 
-  return <div>{optionItems}</div>;
+  return <Row>{optionItems}</Row>;
 }
--- src/pages/entry/tests/Options.test.jsx
+++ src/pages/entry/tests/Options.test.jsx
@@ -9,5 +9,19 @@ test('displays image for each scoop option from server', async () => {
   expect(scoopImages).toHaveLength(2);
 
   const altText = scoopImages.map((element) => element.alt);
-  expect(altText).toEqual().toEqual(['Chocolate scoop', 'Vanilla scoop']);
+  expect(altText).toEqual(['Chocolate scoop', 'Vanilla scoop']);
+});
+
+test('displays image for each topping option from server', async () => {
+  render(<Options optionType="toppings" />);
+
+  const images = await screen.findAllByRole('img', { name: /topping$/i });
+  expect(images).toHaveLength(3);
+
+  const imageTitles = images.map((img) => img.alt);
+  expect(imageTitles).toEqual([
+    'Cherries topping',
+    'M&Ms topping',
+    'Hot fudge topping'
+  ])
 })
