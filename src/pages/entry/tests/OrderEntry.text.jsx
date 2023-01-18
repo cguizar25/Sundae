c5960d715ab0ad8451dbb59cf22dc0009e4b0455
Christian <c-guizar@hotmail.com>
2023-01-17 22:28:35 -0800

    fix(orderentry): added a waitfor for orderEntry test

--- src/pages/entry/tests/OrderEntry.text.jsx
+++ src/pages/entry/tests/OrderEntry.text.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import OrderEntry from '../OrderEntry';
 import { rest } from 'msw';
 import { server } from '../../../mocks/server';
@@ -14,9 +14,11 @@ test('handles errors for scoops and toppings routes', async () =>{
   );
 
   render(<OrderEntry />);
-  const alerts = screen.findAllByRole('alert', {
-    name: 'An unexpected error ocurred. Please try again later.',
-  });
 
-  expect(alerts).toHaveLength(2);
+  await waitFor(async() => {
+    const alerts = screen.findAllByRole('alert');
+    expect(alerts).toHaveLength(2);
+  })
+
+
 })
